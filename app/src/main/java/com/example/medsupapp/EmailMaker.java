faee0239f6bc9e3b91062fdbb2ee23f308a53a3d
Efeighery <eoghanrcfeighery@gmail.com>
2023-02-22 12:00:05 +0000

    New additions

--- app/src/main/java/com/example/medsupapp/EmailMaker.java
+++ app/src/main/java/com/example/medsupapp/EmailMaker.java
@@ -2,13 +2,61 @@ package com.example.medsupapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+/*
+ *  Class name: EmailMaker.java
+ *
+ *  Version: Revision 1
+ *
+ *  Date e.g. 01/02/2023
+ *
+ * @author Eoghan Feighery, x19413886
+ *
+ */
+
+/*
+ *
+ * @reference: https://www.youtube.com/watch?v=CUfACYmqjo4/EmailMaker.java
+ *
+ */
 
 public class EmailMaker extends AppCompatActivity {
+
+    EditText emailRec, emailSubject, emailMessage;
+    Button sendBtn, returnBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_email_maker);
+
+        emailRec = findViewById(R.id.emailAdd);
+        emailSubject = findViewById(R.id.emailSubject);
+        emailMessage = findViewById(R.id.emailContent);
+        sendBtn = findViewById(R.id.sendEmailBtn);
+
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String recipient = emailRec.getText().toString();
+                String sbj = emailSubject.getText().toString();
+                String messageBox = emailMessage.getText().toString();
+
+                Intent itt = new Intent(Intent.ACTION_SEND);
+                itt.putExtra(Intent.EXTRA_EMAIL, new String []{recipient});
+                itt.putExtra(Intent.EXTRA_SUBJECT, sbj);
+                itt.putExtra(Intent.EXTRA_TEXT, messageBox);
+
+                itt.setType("message/rfc822");
+
+                startActivity(Intent.createChooser(itt, "Select an email address"));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/medsupapp/TextMaker.java
+++ app/src/main/java/com/example/medsupapp/TextMaker.java
@@ -2,13 +2,74 @@ package com.example.medsupapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/*
+ *  Class name: TextMaker.java
+ *
+ *  Version: Revision 1
+ *
+ *  Date e.g. 01/02/2023
+ *
+ * @author Eoghan Feighery, x19413886
+ *
+ */
+
+/*
+ *
+ * @reference:  https://www.youtube.com/watch?v=ofAL1C4jUJw/TextMaker.java
+ *
+ */
 
 public class TextMaker extends AppCompatActivity {
+
+    private EditText medNumber, medMessage;
+    private Button sendMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_maker);
+
+        medNumber = findViewById(R.id.phoneNo);
+        medMessage = findViewById(R.id.textMsg);
+        sendMessage = findViewById(R.id.sendTextBtn);
+
+        sendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    if(checkSelfPermission(android.Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED){
+                        sendSMS();
+                    }
+                    else{
+                        requestPermissions(new String[]{Manifest.permission.SEND_SMS}, 1);
+                    }
+                }
+            }
+        });
+    }
+
+    private void sendSMS(){
+        String phoneNum = medNumber.getText().toString();
+        String SMSMessage = medMessage.getText().toString();
+
+        try{
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNum, null, SMSMessage, null, null);
+            Toast.makeText(this, "Message delivered", Toast.LENGTH_SHORT).show();
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+            Toast.makeText(this, "Couldn't send your message", Toast.LENGTH_SHORT).show();
+        }
     }
 }
