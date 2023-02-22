d96fec2c4573fe4886f091097c83d673a6b50512
Efeighery <eoghanrcfeighery@gmail.com>
2023-02-22 16:33:00 +0000

    New additions

--- app/src/main/java/com/example/medsupapp/AddContact.java
+++ app/src/main/java/com/example/medsupapp/AddContact.java
@@ -1,18 +1,30 @@
 package com.example.medsupapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Date;
 /*
  *  Class name: AddContact.java
  *
@@ -53,14 +65,6 @@ public class AddContact extends AppCompatActivity {
 
         contactItemArrayList = new ArrayList<>();
 
-        addCon = findViewById(R.id.addBtn);
-        addCon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
         homeBtn = findViewById(R.id.homeBtn);
         homeBtn.setOnClickListener(v -> startActivity(new Intent(AddContact.this, MainActivity.class)));
 
@@ -69,5 +73,89 @@ public class AddContact extends AppCompatActivity {
 
         smsBtn = findViewById(R.id.textBtn);
         smsBtn.setOnClickListener(v -> startActivity(new Intent(AddContact.this, TextMaker.class)));
+
+        addCon = findViewById(R.id.addBtn);
+        addCon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ViewDialogAddContact viewDialogAddContact = new ViewDialogAddContact();
+                viewDialogAddContact.ShowDialog(AddContact.this);
+            }
+        });
+        
+        displayContacts();
+
+        
+    }
+
+    private void displayContacts() {
+        databaseReference.child("CONTACTS").orderByChild("cID").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                contactItemArrayList.clear();
+
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    ContactItem contactItem = dataSnapshot.getValue(ContactItem.class);
+                    contactItemArrayList.add(contactItem);
+                }
+
+                adapter = new ContactRecyclerAdapter(AddContact.this, contactItemArrayList);
+                recyclerView.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    public class ViewDialogAddContact{
+        public void ShowDialog(Context context){
+            final Dialog dia = new Dialog(context);
+
+            dia.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dia.setCancelable(false);
+
+            dia.setContentView(R.layout.alert_dialog_add_new_contact);
+
+            EditText cName = dia.findViewById(R.id.conName);
+            EditText cEmail = dia.findViewById(R.id.conEmailAdd);
+            EditText cPhone = dia.findViewById(R.id.conTelephone);
+
+            Button addBotn = dia.findViewById(R.id.addCon);
+            Button exitBtn = dia.findViewById(R.id.cancelAction);
+
+            exitBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dia.dismiss();
+                }
+            });
+
+            addBotn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String cID = "Contact"+ new Date().getTime();
+                    String name = cName.getText().toString();
+                    String email = cEmail.getText().toString();
+                    String phone = cPhone.getText().toString();
+
+                    if(name.isEmpty() || email.isEmpty() || phone.isEmpty()){
+                        Toast.makeText(context, "All fields have to be filled in to save a contact entry", Toast.LENGTH_LONG).show();
+                    }
+                    else{
+                        databaseReference.child("CONTACTS").child(cID).setValue(new ContactItem(cID, name, email, phone));
+                        Toast.makeText(context, "And....Voila!", Toast.LENGTH_LONG).show();
+                        dia.dismiss();
+                    }
+                }
+            });
+
+            dia.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            dia.show();
+        }
     }
 }
--- app/src/main/java/com/example/medsupapp/ContactRecyclerAdapter.java
+++ app/src/main/java/com/example/medsupapp/ContactRecyclerAdapter.java
@@ -17,10 +17,18 @@ package com.example.medsupapp;
  *
  */
 
+import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -38,7 +46,7 @@ public class ContactRecyclerAdapter extends RecyclerView.Adapter<ContactRecycler
 
     DatabaseReference databaseReference;
 
-    public ContactRecyclerAdapter(Context context, ArrayList<ContactItem> contactItemArrayList, DatabaseReference databaseReference) {
+    public ContactRecyclerAdapter(Context context, ArrayList<ContactItem> contactItemArrayList) {
         this.context = context;
         this.contactItemArrayList = contactItemArrayList;
         databaseReference = FirebaseDatabase.getInstance().getReference();
@@ -56,7 +64,110 @@ public class ContactRecyclerAdapter extends RecyclerView.Adapter<ContactRecycler
 
     @Override
     public void onBindViewHolder(@NonNull ContactRecyclerAdapter.ViewHolder holder, int position) {
+        ContactItem contactItem = contactItemArrayList.get(position);
 
+        holder.conName.setText("Name: "+contactItem.getName());
+        holder.conEmailAd.setText("Name: "+contactItem.getEmail());
+        holder.conPhone.setText("Name: "+contactItem.getPhone());
+
+        holder.editCon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ViewDialogContactEdit viewDialogContactEdit = new ViewDialogContactEdit();
+                viewDialogContactEdit.showDialog(context, contactItem.getcID(), contactItem.getName(), contactItem.getEmail(), contactItem.getPhone());
+            }
+        });
+
+        holder.delCon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ViewDialogContactRemoval viewDialogContactRemoval = new ViewDialogContactRemoval();
+                viewDialogContactRemoval.showDialog(context, contactItem.getcID());
+            }
+        });
+
+
+
+    }
+
+    public class ViewDialogContactEdit{
+        public void showDialog(Context context, String cID, String name, String email, String phone){
+
+            final Dialog dialog = new Dialog(context);
+            dialog.setCancelable(false);
+            dialog.setContentView(R.layout.alert_dialog_add_new_contact);
+
+            EditText conName = dialog.findViewById(R.id.conName);
+            EditText conEmailAd = dialog.findViewById(R.id.conEmailAdd);
+            EditText conPhone = dialog.findViewById(R.id.conTelephone);
+
+            conName.setText(name);
+            conEmailAd.setText(email);
+            conPhone.setText(phone);
+
+            Button updateContact = dialog.findViewById(R.id.addCon);
+            Button exitField = dialog.findViewById(R.id.cancelAction);
+
+            updateContact.setText("Edit contact");
+
+            exitField.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String alteredName = conName.getText().toString();
+                    String alteredEmail = conEmailAd.getText().toString();
+                    String alteredPhone = conPhone.getText().toString();
+
+                    if(name.isEmpty() || email.isEmpty() || phone.isEmpty()){
+                        Toast.makeText(context, "All fields must be filled!", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        if(alteredName.equals(name) && alteredEmail.equals(email) && alteredPhone.equals(phone)){
+                            Toast.makeText(context, "Hmm, no edits made", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            databaseReference.child("contact").child(cID).setValue(new ContactItem(cID, name, email, phone));
+                            Toast.makeText(context, "And the user is updated now", Toast.LENGTH_LONG).show();
+                            dialog.dismiss();
+                        }
+                    }
+                }
+            });
+
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            dialog.show();
+        }
+    }
+
+    public class ViewDialogContactRemoval{
+        public void showDialog(Context context, String cID){
+            final Dialog dialog = new Dialog(context);
+
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.setCancelable(false);
+
+            dialog.setContentView(R.layout.view_dialog_remove_contact);
+
+            Button removeCon = dialog.findViewById(R.id.delBtn);
+            Button exitBtn = dialog.findViewById(R.id.cancelBtn);
+
+            exitBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                }
+            });
+
+            removeCon.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    databaseReference.child("CONTACTS").child(cID).removeValue();
+                    Toast.makeText(context, "Entry removed", Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                }
+            });
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            dialog.show();
+        }
     }
 
     @Override
@@ -65,9 +176,19 @@ public class ContactRecyclerAdapter extends RecyclerView.Adapter<ContactRecycler
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView conName, conPhone, conEmailAd;
+
+        Button delCon, editCon;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
+            conName = itemView.findViewById(R.id.nameCon);
+            conPhone = itemView.findViewById(R.id.phoneCon);
+            conEmailAd = itemView.findViewById(R.id.emailCon);
+
+            delCon = itemView.findViewById(R.id.removeBtn);
+            editCon = itemView.findViewById(R.id.updateBtn);
 
         }
     }
