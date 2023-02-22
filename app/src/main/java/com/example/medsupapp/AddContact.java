fdd136eedc9f824f8b458b8951ed4d35888854ad
Efeighery <eoghanrcfeighery@gmail.com>
2023-02-22 11:12:44 +0000

    New additions

--- app/src/main/java/com/example/medsupapp/AddContact.java
+++ app/src/main/java/com/example/medsupapp/AddContact.java
@@ -1,12 +1,16 @@
 package com.example.medsupapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 /*
@@ -40,5 +44,30 @@ public class AddContact extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contact);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        recyclerView = findViewById(R.id.reView);
+        recyclerView.setHasFixedSize(false);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        contactItemArrayList = new ArrayList<>();
+
+        addCon = findViewById(R.id.addBtn);
+        addCon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        homeBtn = findViewById(R.id.homeBtn);
+        homeBtn.setOnClickListener(v -> startActivity(new Intent(AddContact.this, MainActivity.class)));
+
+        emailBtn = findViewById(R.id.emailBtn);
+        emailBtn.setOnClickListener(v -> startActivity(new Intent(AddContact.this, EmailMaker.class)));
+
+        smsBtn = findViewById(R.id.textBtn);
+        smsBtn.setOnClickListener(v -> startActivity(new Intent(AddContact.this, TextMaker.class)));
     }
 }
