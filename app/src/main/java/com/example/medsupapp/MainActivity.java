e1a094ed4b39a80ef2463bbebba381928681adff
Efeighery <eoghanrcfeighery@gmail.com>
2023-02-27 17:10:17 +0000

    Added navigation to Text and Email Maker pages to the home page

--- app/src/main/java/com/example/medsupapp/MainActivity.java
+++ app/src/main/java/com/example/medsupapp/MainActivity.java
@@ -6,11 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     // The function buttons are declared and initialised as well for logging out
     Button medicationBtn, recorderBtn, calBtn, conBtn, conditionBtn, userProBtn;
+
+    ImageView textMsg, emailMak;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,13 @@ public class MainActivity extends AppCompatActivity {
         conBtn = findViewById(R.id.contactBtn);
         conditionBtn = findViewById(R.id.healthBtn);
         userProBtn = findViewById(R.id.profileBtn);
+
+        textMsg = findViewById(R.id.smsBtn);
+        emailMak = findViewById(R.id.emailBox);
+
+        textMsg.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, TextMaker.class)));
+
+        emailMak.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, EmailMaker.class)));
 
         // Whenever any of these buttons are clicked, the page will be changed to the corresponding one
         recorderBtn.setOnClickListener(v -> {
