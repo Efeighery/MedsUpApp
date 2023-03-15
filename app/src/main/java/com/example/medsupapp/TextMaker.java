607ef4afc3886e1f9c5770525f264dc83b442be6
Efeighery <eoghanrcfeighery@gmail.com>
2023-03-15 18:09:21 +0000

    Altered TextMaker to utilise WhatsApp

--- app/src/main/java/com/example/medsupapp/TextMaker.java
+++ app/src/main/java/com/example/medsupapp/TextMaker.java
@@ -28,13 +28,13 @@ import android.widget.Toast;
 
 /*
  *
- * @reference:  https://www.javatpoint.com/how-to-send-sms-in-android/TextMaker.java
+ * @reference:  https://www.geeksforgeeks.org/how-to-send-message-on-whatsapp-in-android/TextMaker.java
  *
  */
 
 public class TextMaker extends AppCompatActivity {
 
-    private EditText medNumber, medMessage;
+    private EditText medMessage;
     private Button sendMessage, home;
 
     @Override
@@ -42,7 +42,6 @@ public class TextMaker extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_maker);
 
-        medNumber = findViewById(R.id.phoneNo);
         medMessage = findViewById(R.id.textMsg);
         sendMessage = findViewById(R.id.sendTextBtn);
         home = findViewById(R.id.returnConBtn);
@@ -50,20 +49,37 @@ public class TextMaker extends AppCompatActivity {
         sendMessage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String phoneNo = medNumber.getText().toString();
                 String message = medMessage.getText().toString();
 
-                Intent intent = new Intent(getApplicationContext(), TextMaker.class);
-                PendingIntent pen = PendingIntent.getActivity(getApplicationContext(), 0, intent, 0);
-
-                SmsManager smsManager = SmsManager.getDefault();
-                smsManager.sendTextMessage(phoneNo, null, message, pen, null);
-
-                Toast.makeText(TextMaker.this, "Message sending complete", Toast.LENGTH_SHORT).show();
+                // Activates this method after the message is obtained from the text field
+                sendMessageIntent(message);
             }
         });
 
         home.setOnClickListener(v -> startActivity(new Intent(TextMaker.this, MainActivity.class)));
+    }
+
+    // This method uses an Intent object to send a message via WhatsApp
+    private void sendMessageIntent(String message){
+
+        // This will set the Intent action to send a message
+        Intent in = new Intent(Intent.ACTION_SEND);
+
+        // The Intent will be declared as a plain text type of message and the package will be set to check if WhatsApp has been installed
+        in.setType("text/plain");
+        in.setPackage("com.whatsapp");
+
+        // This will save the message when the app transitions to WhatsApp
+        in.putExtra(Intent.EXTRA_TEXT, message);
+
+        // If WhatsApp isn't in the user's phone, this message will appear instead
+        if(in.resolveActivity(getPackageManager()) == null){
+            Toast.makeText(TextMaker.this, "Install WhatsApp on your phone first", Toast.LENGTH_SHORT).show();
+
+            return;
+        }
+
+        startActivity(in);
     }
 
 }
