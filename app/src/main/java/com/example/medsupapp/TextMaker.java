9161ee1af94bd137ab7632024ca2f6791b13bc00
Efeighery <eoghanrcfeighery@gmail.com>
2023-03-15 22:53:18 +0000

    Altered TextMaker to utilise WhatsApp and added permission for Alarm

--- app/src/main/java/com/example/medsupapp/TextMaker.java
+++ app/src/main/java/com/example/medsupapp/TextMaker.java
@@ -28,58 +28,39 @@ import android.widget.Toast;
 
 /*
  *
- * @reference:  https://www.geeksforgeeks.org/how-to-send-message-on-whatsapp-in-android/TextMaker.java
+ * @reference:  https://www.youtube.com/watch?v=wDl1ZsCKBtI&t=184s/TextMaker.java
  *
  */
 
 public class TextMaker extends AppCompatActivity {
 
-    private EditText medMessage;
     private Button sendMessage, home;
+    private String notice = "Message can be made in WhatsApp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_maker);
 
-        medMessage = findViewById(R.id.textMsg);
+
         sendMessage = findViewById(R.id.sendTextBtn);
         home = findViewById(R.id.returnConBtn);
 
         sendMessage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String message = medMessage.getText().toString();
+                Intent whatsApp = new Intent(Intent.ACTION_SEND);
 
-                // Activates this method after the message is obtained from the text field
-                sendMessageIntent(message);
+                whatsApp.putExtra(Intent.EXTRA_TEXT, notice);
+                whatsApp.setType("text/plain");
+
+                whatsApp.setPackage("com.whatsapp");
+                startActivity(whatsApp);
             }
+
         });
 
         home.setOnClickListener(v -> startActivity(new Intent(TextMaker.this, MainActivity.class)));
-    }
-
-    // This method uses an Intent object to send a message via WhatsApp
-    private void sendMessageIntent(String message){
-
-        // This will set the Intent action to send a message
-        Intent in = new Intent(Intent.ACTION_SEND);
-
-        // The Intent will be declared as a plain text type of message and the package will be set to check if WhatsApp has been installed
-        in.setType("text/plain");
-        in.setPackage("com.whatsapp");
-
-        // This will save the message when the app transitions to WhatsApp
-        in.putExtra(Intent.EXTRA_TEXT, message);
-
-        // If WhatsApp isn't in the user's phone, this message will appear instead
-        if(in.resolveActivity(getPackageManager()) == null){
-            Toast.makeText(TextMaker.this, "Install WhatsApp on your phone first", Toast.LENGTH_SHORT).show();
-
-            return;
-        }
-
-        startActivity(in);
     }
 
 }
